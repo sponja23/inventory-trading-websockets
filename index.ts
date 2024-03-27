471e1a0d91ed80a2d36f53e17a423af682cd987e
sponja23 <tomas.spognardi@gmail.com>
2024-03-26 23:39:21 -0300

    feat(client): Added test client

--- index.ts
+++ index.ts
@@ -1,3 +1,16 @@
 import { Server } from "socket.io";
 
-const io = new Server(3000);
+const io = new Server(3000, {
+    cors: {
+        // Any origin is allowed (for now)
+        origin: "*",
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
+});
