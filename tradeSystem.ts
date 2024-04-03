6670d8bda8265cacbee68f394daabba9538f1191
sponja23 <tomas.spognardi@gmail.com>
2024-04-02 21:45:04 -0300

    fix: Changed handling of user actions, and added server actions list

--- tradeSystem.ts
+++ tradeSystem.ts
@@ -4,115 +4,144 @@ import { DefaultEventsMap } from "socket.io/dist/typed-events";
 export type Inventory = string[];
 
 export type TradeInformation = {
-    name1: string;
-    name2: string;
+    userId1: string;
+    userId2: string;
     inventory1: Inventory;
     inventory2: Inventory;
 };
 
 enum UserState {
-    noName,
+    noUserId,
     inLobby,
     sentInvite,
-    receivedInvite,
     inTrade,
     lockedIn,
 }
 
 type SocketData = {
-    name?: string;
+    userId?: string;
     state: UserState;
+    inviteSentTo?: string;
     tradeInfo?: TradeInformation;
 };
 
 type UserActions = {
-    setName: (name: string) => void;
-    sendInvite: (name: string) => void;
-    cancelInvite: () => void;
-    acceptInvite: () => void;
-    rejectInvite: () => void;
+    // Send the credentials to the server, validate them, and authenticate the user
+    authenticate: (userId: string) => void;
+    // Send an invite to another user
+    sendInvite: (userId: string) => void;
+    // Cancel an invite that was sent to another user
+    cancelInvite: (userId: string) => void;
+    // Accept an invite from another user
+    acceptInvite: (userId: string) => void;
+    // Reject an invite from another user
+    rejectInvite: (userId: string) => void;
+    // Update the inventory that is being traded
     updateInventory: (inventory: Inventory) => void;
+    // Lock in the inventory that is being traded
     lockIn: (inventory1: Inventory, inventory2: Inventory) => void;
+    // Undo the lock-in of the inventory that is being traded
+    unlock: () => void;
+    // Cancel the trade
     cancelTrade: () => void;
 };
 
-const actionList: (keyof UserActions)[] = [
-    "setName",
+const userActionList = [
+    "authenticate",
     "sendInvite",
     "cancelInvite",
     "acceptInvite",
     "rejectInvite",
     "updateInventory",
     "lockIn",
+    "unlock",
     "cancelTrade",
-];
+] as (keyof UserActions)[];
+
+type ServerActions = {
+    // Notify the user that the action was successful
+    success: (message: string) => void;
+    // Notify the user that the action was unsuccessful
+    error: (message: string) => void;
+    // Notify the user that they have received an invite
+    inviteReceived: (userId: string) => void;
+    // Notify the user that the invite was accepted
+    inviteAccepted: (userId: string) => void;
+    // Notify the user that the invite was rejected
+    inviteRejected: (userId: string) => void;
+    // Notify the user that the trade inventory was updated
+    inventoryUpdated: (inventory: Inventory) => void;
+    // Notify the user that the trade was locked in
+    lockedIn: (inventory1: Inventory, inventory2: Inventory) => void;
+    // Notify the user that the trade was unlocked
+    unlocked: () => void;
+    // Notify the user that the trade was cancelled
+    tradeCancelled: () => void;
+    // Notify the user that the trade was completed
+    tradeCompleted: () => void;
+};
+
+type TradeSocket = Socket<
+    UserActions,
+    DefaultEventsMap,
+    Record<string, never>,
+    SocketData
+>;
+
+const validActions = new Map<UserState, (keyof UserActions)[]>([
+    [UserState.noUserId, ["authenticate"]],
+    [UserState.inLobby, ["sendInvite", "acceptInvite", "rejectInvite"]],
+    [UserState.sentInvite, ["cancelInvite", "acceptInvite", "rejectInvite"]],
+    [UserState.inTrade, ["updateInventory", "lockIn", "cancelTrade"]],
+    [UserState.lockedIn, ["unlock"]],
+]);
 
 export class TradeSystem {
-    private nameToSocket = new Map<
-        string,
-        Socket<UserActions, DefaultEventsMap>
-    >();
-    private transitions = new Map<
-        UserState,
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        Map<keyof UserActions, (socket: Socket, ...args: any[]) => void>
-    >();
+    // userId -> socket
+    private userIdToSocket = new Map<string, TradeSocket>();
+    // userId -> [invites received]
+    private pendingInvites = new Map<string, string[]>();
 
     constructor(
         private io: Server<
             UserActions,
-            DefaultEventsMap,
+            ServerActions,
             Record<string, never>,
             SocketData
         >,
     ) {
-        this.setupTransitions();
         this.setupSocketEvents();
     }
 
-    private setupTransitions() {
-        this.transitions.set(
-            UserState.noName,
-            new Map([["setName", this.handleSetName.bind(this)]]),
-        );
-        this.transitions.set(
-            UserState.inLobby,
-            new Map([["sendInvite", this.handleSendInvite.bind(this)]]),
-        );
-        this.transitions.set(
-            UserState.sentInvite,
-            new Map([["cancelInvite", this.handleCancelInvite.bind(this)]]),
-        );
-        this.transitions.set(
-            UserState.receivedInvite,
-            new Map([
-                ["acceptInvite", this.handleAcceptInvite.bind(this)],
-                ["rejectInvite", this.handleRejectInvite.bind(this)],
-            ]),
-        );
-        this.transitions.set(
-            UserState.inTrade,
-            new Map([
-                ["updateInventory", this.handleUpdateInventory.bind(this)],
-                ["lockIn", this.handleLockIn.bind(this)],
-                ["cancelTrade", this.handleCancelTrade.bind(this)],
-            ]),
-        );
-    }
-
     private setupSocketEvents() {
+        const handlers = new Map<
+            keyof UserActions,
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            (socket: TradeSocket, ...args: any) => UserState
+        >([
+            ["authenticate", this.handleAuthenticate],
+            ["sendInvite", this.handleSendInvite],
+            ["cancelInvite", this.handleCancelInvite],
+            ["acceptInvite", this.handleAcceptInvite],
+            ["rejectInvite", this.handleRejectInvite],
+            ["updateInventory", this.handleUpdateInventory],
+            ["lockIn", this.handleLockIn],
+            ["unlock", this.handleUnlock],
+            ["cancelTrade", this.handleCancelTrade],
+        ]);
+
         this.io.on("connection", (socket) => {
             // Set initial state
-            socket.data.state = UserState.noName;
+            socket.data.state = UserState.noUserId;
 
             // Iterate over actions (keys of UserActions)
-            for (const action of actionList) {
+            for (const action of userActionList) {
                 // eslint-disable-next-line @typescript-eslint/no-explicit-any
                 socket.on(action, (...args: any): void => {
                     const state = socket.data.state;
-                    const transition = this.transitions.get(state)?.get(action);
-                    if (transition) {
-                        transition(socket, ...args);
+                    const handler = handlers.get(action);
+                    if (handler) {
+                        handler(socket, ...args);
                     } else {
                         socket.emit(
                             "error",
@@ -123,57 +152,68 @@ export class TradeSystem {
             }
 
             socket.on("disconnect", () => {
-                if (socket.data.name) {
-                    this.nameToSocket.delete(socket.data.name);
-                    console.log(`User "${socket.data.name}" has disconnected`);
+                if (socket.data.userId) {
+                    this.userIdToSocket.delete(socket.data.userId);
+                    console.log(
+                        `User "${socket.data.userId}" has disconnected`,
+                    );
                 }
             });
         });
     }
 
-    private handleSetName(socket: Socket, name: string) {
-        if (this.nameToSocket.has(name)) {
-            socket.emit("error", `Name "${name}" is already taken`);
-            return;
+    private handleAuthenticate(socket: TradeSocket, userId: string): UserState {
+        if (this.userIdToSocket.has(userId)) {
+            socket.emit("error", `User ID "${userId}" is already connected`);
+            return UserState.noUserId;
         }
 
-        this.nameToSocket.set(name, socket);
-        socket.data.name = name;
+        this.userIdToSocket.set(userId, socket);
+        socket.data.userId = userId;
         socket.data.state = UserState.inLobby;
-        socket.emit("success", name);
+        socket.emit("success", userId);
 
-        console.log(`User "${name}" has connected`);
+        console.log(`User "${userId}" has connected`);
+
+        return UserState.inLobby;
     }
 
-    private handleSendInvite(socket: Socket, name: string) {
+    private handleSendInvite(socket: TradeSocket, userId: string): UserState {
         // TODO
     }
 
-    private handleCancelInvite(socket: Socket) {
+    private handleCancelInvite(socket: TradeSocket): UserState {
         // TODO
     }
 
-    private handleAcceptInvite(socket: Socket) {
+    private handleAcceptInvite(socket: TradeSocket): UserState {
         // TODO
     }
 
-    private handleRejectInvite(socket: Socket) {
+    private handleRejectInvite(socket: TradeSocket): UserState {
         // TODO
     }
 
-    private handleUpdateInventory(socket: Socket, inventory: Inventory) {
+    private handleUpdateInventory(
+        socket: TradeSocket,
+        inventory: Inventory,
+    ): UserState {
         // TODO
     }
 
     private handleLockIn(
-        socket: Socket,
+        socket: TradeSocket,
         inventory1: Inventory,
         inventory2: Inventory,
-    ) {
+    ): UserState {
         // TODO
     }
 
-    private handleCancelTrade(socket: Socket) {
+    private handleUnlock(socket: TradeSocket): UserState {
+        // TODO
+    }
+
+    private handleCancelTrade(socket: TradeSocket): UserState {
         // TODO
     }
 }
