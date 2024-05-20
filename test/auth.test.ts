b655e4621afdb950624cfef8de304f17eba67ba2
sponja23 <tomas.spognardi@gmail.com>
2024-05-20 02:45:33 -0300

    refactor(tests): Refactored setup into TradeServerTestHarness class

--- test/auth.test.ts
+++ test/auth.test.ts
@@ -1,64 +1,13 @@
-import {
-    describe,
-    beforeAll,
-    afterAll,
-    test,
-    expect,
-    beforeEach,
-    afterEach,
-} from "@jest/globals";
-import { TradeServer } from "../src/tradeServer";
+import { describe, test, expect } from "@jest/globals";
 import { UserState } from "../src/types";
-
-import http from "http";
-import { Server } from "socket.io";
-import ioc, { Socket } from "socket.io-client";
-import { AddressInfo } from "net";
-
-let httpServer: http.Server;
-let serverAddress: AddressInfo;
-let tradeSystem: TradeServer;
-
-beforeAll((done) => {
-    httpServer = http.createServer();
-    serverAddress = httpServer.listen().address() as AddressInfo;
-
-    tradeSystem = new TradeServer(new Server(httpServer));
-
-    done();
-});
-
-afterAll((done) => {
-    tradeSystem.close();
-    httpServer.close();
-
-    done();
-});
-
-let client: Socket;
-
-beforeEach((done) => {
-    client = ioc(`http://[${serverAddress.address}]:${serverAddress.port}`, {
-        multiplex: false,
-    });
-
-    client.on("connect", () => {
-        client.emit("authenticate", "test-user", () => {
-            done();
-        });
-    });
-});
-
-afterEach((done) => {
-    // Cleanup
-    if (client.connected) {
-        client.disconnect();
-    }
-    done();
-});
+import { TradeServerTestHarness } from "./utils";
 
 describe("Authentication Tests", () => {
+    const harness = new TradeServerTestHarness(["test-user"]);
+
     test("Authenticated user is in lobby", () => {
-        expect(tradeSystem.getUserState("test-user")).toBe(UserState.inLobby);
+        expect(harness.tradeSystem!.getUserState("test-user")).toBe(
+            UserState.inLobby,
+        );
     });
 });
---
+++ test/utils.ts
@@ -0,0 +1,62 @@
+import http from "http";
+import { Server } from "socket.io";
+import ioc, { Socket } from "socket.io-client";
+import { AddressInfo } from "net";
+import { TradeServer } from "../src/tradeServer";
+import { afterAll, afterEach, beforeAll, beforeEach } from "@jest/globals";
+
+export class TradeServerTestHarness {
+    private httpServer: http.Server | null = null;
+    private serverAddress: AddressInfo | null = null;
+    public tradeSystem: TradeServer | null = null;
+    public clients: (Socket | null)[] = [];
+
+    constructor(clientNames: string[]) {
+        this.clients = new Array(clientNames.length).fill(null);
+
+        beforeAll((done) => {
+            this.httpServer = http.createServer();
+            this.serverAddress = this.httpServer
+                .listen()
+                .address() as AddressInfo;
+
+            this.tradeSystem = new TradeServer(new Server(this.httpServer));
+
+            done();
+        });
+
+        afterAll((done) => {
+            this.tradeSystem!.close();
+            this.httpServer!.close();
+
+            done();
+        });
+
+        clientNames.forEach((name, i) => {
+            beforeEach((done) => {
+                const client = ioc(
+                    `http://[${this.serverAddress!.address}]:${this.serverAddress!.port}`,
+                    {
+                        multiplex: false,
+                    },
+                );
+
+                client.on("connect", () => {
+                    client.emit("authenticate", name, () => {
+                        done();
+                    });
+                });
+
+                this.clients[i] = client;
+            });
+
+            afterEach((done) => {
+                if (this.clients[i]!.connected) {
+                    this.clients[i]!.disconnect();
+                }
+
+                done();
+            });
+        });
+    }
+}
